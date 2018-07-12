29edb73feaf5ee3f2552289b4059a01a35dd6d62
julian gonzalez <jagonzalezv9826@gmail.com>
2018-07-11 22:25:45 -0500

    archivos landing

---
+++ landing.php
@@ -0,0 +1,331 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8" />
+        <title>LANDING | TRADING CRIPTOMONEDAS | BITCOIN, LITECOIN, ETERIUM</title>
+        <meta name="description" content="LANDING | TRADING CRIPTOMONEDAS | BITCOIN, LITECOIN, ETERIUM">
+        <meta name="author" content="www.creasotol.com, contactos@creasotol.com">
+        <meta name="Subject" content="LANDING | TRADING CRIPTOMONEDAS | BITCOIN, LITECOIN, ETERIUM">
+        <meta name="robots" content="index,follow">
+        <meta NAME="Language" CONTENT="Spanish">
+        <meta NAME="Revisit" CONTENT="1 day">
+        <meta NAME="Distribution" CONTENT="Global">
+        <!--para desactivar el cache de todos los navegadores-->
+        <meta http-equiv="cache-control" content="max-age=0" />
+        <meta http-equiv="cache-control" content="no-cache, no-store, must-revalidate" />
+        <meta http-equiv="pragma" content="no-cache" />
+        <meta http-equiv="Expires" content="0">
+        <meta http-equiv="Last-Modified" content="0">
+        <!--cierra-->
+        <!-- Mobile Specific Metas
+        ================================================== -->
+        
+        <?php include 'partials/scriptsGlobal.php';  ?>
+
+    </head>
+    <body id="" class="">                
+                
+
+        <div id="pageLanding" class="disTable pageLanding">
+
+            <div class="secc-header">
+                <div class="arriba disTable">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12 col-sm-12 col-xs-12">
+                                <?php include 'partials/redes.php'; ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="abajo disTable">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12 col-sm-12 col-xs-12">
+                                <div class="contimg disTable">
+                                    <img src="public/img/HEADER/1.png" alt="TRADING CRIPTOMONEDAS | BITCOIN, LITECOIN, ETERIUM">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="secc-seminarioprin disTable">
+                <div class="container">
+                    <div class="row">
+                        <div class="informacion disTable">
+                            <div class="arriba disTable">
+                                <div class="col-md-12 col-sm-12 col-xs-12">
+                                    <h1>SEMINARIO PROFESIONAL DE CRIPTOMONEDAS 2018</h1>
+                                    <h2>OPORTUNIDADES Y FUNDAMENTOS</h2>
+                                </div>
+                            </div>
+                            <div class="contimg disTable img-full">
+                                <img src="public/img/imagenes/1.png" alt="">
+                            </div>
+                            <div class="abajo disTable">
+                                <h4>Del 26 al 31 de Enero de 2018</h4>
+                                <h4>Entrada libre - Cupos limitados</h4>
+                            </div>
+                        </div>                        
+                    </div>
+                </div>
+            </div>
+
+            <div class="secc-registro disTable">
+                <div class="container">
+                    <div class="row">    
+                        <div class="informacion disTable">
+                            <form action="" class="form-contacto contacto" method="POST">
+                                <div class="centrado">
+                                    <div class="col-md-12 col-sm-12 col-xs-12">
+                                        <h2>REGISTRATE</h2>
+                                    </div>
+                                    
+                                    <div class="col-md-12 col-sm-12 col-xs-12">
+                                        <div class="form-group">
+                                            <input type="text" class="form-control" placeholder="Nombres y Apellidos *" required>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12 col-sm-12 col-xs-12">
+                                        <div class="form-group">
+                                            <input type="text" class="form-control" placeholder="E-mail *" required>
+                                        </div>
+                                    </div>
+                                    
+                                    <div class="disclaimer col-md-12 col-sm-12 col-xs-12">
+                                        <p>*No enviamos spam ni compartimos su información con terceros*</p>
+                                    </div>
+                                    
+                                    <div class="col-md-12 col-sm-12 col-xs-12">
+                                        <div class="cont-check disTable">
+                                            <div class="checkbox checkbox-circle">
+                                                <input id="checkbox7" type="checkbox" required>
+                                                <label for="checkbox7">
+                                                    Acepto los <a href="terminos.php" target="_blank">Términos y Condiciones</a>.
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                
+
+                                <input value="ENVIAR" class="btnCafe btnBoxShadow " id="btnEnviar" name="btnEnviar" type="submit">
+                            </form>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+
+            <div class="secc-datos disTable">
+                <div class="container">
+                    <div class="row">
+                        <div class="informacion disTable">
+                            <div class="izq col-md-6 col-sm-12 col-xs-12">
+
+                                <div class="experto disTable">
+                                    <div class="col-md-12 col-sm-12 col-xs-12">
+                                        <h3>Experto</h3>
+                                    </div>                                    
+                                    <div class="info disTable">
+                                        <div class="col-md-5 col-sm-12 col-xs-12">
+                                            <div class="contimg disTable">
+                                                <img src="public/img/imagenes/2.png" >
+                                            </div>
+                                        </div>
+                                        <div class="col-md-7 col-sm-12 col-xs-12">
+                                            <div class="continfo disTable">
+                                                <h4>Juan Alberto Maldonado</h4>
+                                                <p>CEO de club Capitales y Miembro de Market Techicians Association New York</p>
+                                            </div>
+                                            <div class="contlogo disTable">
+                                                <img src="public/img/imagenes/5.png" alt="">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="multimedia disTable">
+                                    <h3>Multimedia</h3>
+
+                                    <div class="contvideo disTable">
+                                        <h4>LOREM IPSUM SIT AMET</h4>
+                                        <video src="public/videos/video.mp4"
+                                            controls
+                                            width="720" height="480">
+                                            Tu navegador no soporta el elemento video. <a href="public/videos/video.mp4">Intenta descargando este video</a>.
+                                        </video>
+                                    </div>
+
+                                    <div class="contvideo disTable">
+                                        <h4>LOREM IPSUM SIT AMET</h4>
+                                        <video src="public/videos/video.mp4"
+                                            controls
+                                            width="720" height="480">
+                                            Tu navegador no soporta el elemento video. <a href="public/videos/video.mp4">Intenta descargando este video</a>.
+                                        </video>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="der col-md-6 col-sm-12 col-xs-12">
+
+                                <div class="programa disTable">
+                                    <div class="col-md-12 col-sm-12 col-xs-12">
+                                        <h3>Programación</h3>
+                                    </div>
+
+                                    <div class="col-md-12 col-sm-12 col-xs-12">
+                                        <div class="texto disTable">
+                                            <div class="sesion disTable">
+                                                <h4>Sesión I: Viernes 26 de Enero de 2018 - 8:00 AM</h4>
+                                                <h5>Tema: Introducción a las criptomonedas</h5>
+
+                                                <ul>
+                                                    <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </li>
+                                                    <li>Natus, tenetur obcaecati, totam a aliquam iure repellat nemo quia </li>
+                                                    <li>non delectus enim quod ducimus doloremque. Soluta quidem recusandae explicabo nostrum, velit.</li>
+                                                    <li>Natus, tenetur obcaecati, totam a aliquam iure repellat nemo quia </li>
+                                                    <li>non delectus enim quod ducimus doloremque. Soluta quidem recusandae explicabo nostrum, velit.</li>
+                                                </ul>
+                                            </div>
+
+                                            <div class="sesion disTable">
+                                                <h4>Sesión I: Viernes 26 de Enero de 2018 - 8:00 AM</h4>
+                                                <h5>Tema: Introducción a las criptomonedas</h5>
+
+                                                <ul>
+                                                    <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </li>
+                                                    <li>Natus, tenetur obcaecati, totam a aliquam iure repellat nemo quia </li>
+                                                    <li>non delectus enim quod ducimus doloremque. Soluta quidem recusandae explicabo nostrum, velit.</li>
+                                                    <li>Natus, tenetur obcaecati, totam a aliquam iure repellat nemo quia </li>
+                                                    <li>non delectus enim quod ducimus doloremque. Soluta quidem recusandae explicabo nostrum, velit.</li>
+                                                </ul>
+                                            </div>
+
+                                            <div class="sesion disTable">
+                                                <h4>Sesión I: Viernes 26 de Enero de 2018 - 8:00 AM</h4>
+                                                <h5>Tema: Introducción a las criptomonedas</h5>
+
+                                                <ul>
+                                                    <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </li>
+                                                    <li>Natus, tenetur obcaecati, totam a aliquam iure repellat nemo quia </li>
+                                                    <li>non delectus enim quod ducimus doloremque. Soluta quidem recusandae explicabo nostrum, velit.</li>
+                                                    <li>Natus, tenetur obcaecati, totam a aliquam iure repellat nemo quia </li>
+                                                    <li>non delectus enim quod ducimus doloremque. Soluta quidem recusandae explicabo nostrum, velit.</li>
+                                                </ul>
+                                            </div>
+                                            
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                            </div>
+                        </div>
+                        <a href="#" class="btnVerde btnBoxShadow">REGISTRATE YA Y SEPARA TU CUPO</a>
+                    </div>
+                </div>            
+            </div>
+
+            <div class="secc-texto disTable">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12 col-sm-12 col-xs-12">
+                            <div class="texto disTable">
+                                <p>
+                                    <strong>Lorem ipsum dolor sit amet,</strong> consectetur adipisicing elit. Laudantium sequi aspernatur repellendus nesciunt animi praesentium molestiae, ratione natus debitis omnis neque modi quaerat obcaecati mollitia unde sed vitae, consequatur minus? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut maiores esse ducimus reiciendis labore sed, omnis dolor consectetur numquam! Ipsam, asperiores cumque aliquid nostrum quibusdam voluptas laboriosam nemo consequuntur voluptate.
+                                </p>
+                            </div>
+
+                            <div class="texto disTable">
+                                <p>
+                                    <strong>Lorem ipsum dolor sit amet,</strong> consectetur adipisicing elit. Laudantium sequi aspernatur repellendus nesciunt animi praesentium molestiae, ratione natus debitis omnis neque modi quaerat obcaecati mollitia unde sed vitae, consequatur minus? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut maiores esse ducimus reiciendis labore sed, omnis dolor consectetur numquam! Ipsam, asperiores cumque aliquid nostrum quibusdam voluptas laboriosam nemo consequuntur voluptate.
+                                </p>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="secc-footer disTable">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6 col-sm-12 col-xs-12">
+                            <div class="conttexto disTable">
+                                <div class="contimg disTable">
+                                    <img src="public/img/HEADER/1.png" alt="TRADING CRIPTOMONEDAS | BITCOIN, LITECOIN, ETERIUM">
+                                </div>
+                                <div class="texto disTable">
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis nulla est quaerat aut amet non, cupiditate expedita. Distinctio quidem voluptatibus vero, reprehenderit consequatur corrupti fugiat sequi eius quos, tempora corporis.
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-sm-12 col-xs-12">
+                            <div class="informacion disTable">
+                                <div class="titulo disTable">
+                                    <h3>Contactos</h3>
+                                </div>
+                                <div class="dato disTable">
+                                    <i class="fa fa-map-marker" aria-hidden="true"></i>
+                                    <p>Ibagué - Tolima - Colombia</p>
+                                </div>
+                                <div class="dato disTable">
+                                    <i class="fa fa-phone" aria-hidden="true"></i>
+                                    <p><a href="tel:+57 (8) 268 0505">+57 (8) 268 0505</a></p>
+                                </div>
+                                <div class="dato disTable mail">
+                                    <i class="fa fa-envelope" aria-hidden="true"></i>
+                                    <p><a href="mailto:info@tradingmarketcash.com">info@tradingmarketcash.com</a></p>
+                                </div>
+
+                                <div class="dato disTable">
+                                    <?php include 'partials/redes.php'; ?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>            
+
+        </div>  
+
+        <script assync>
+
+            jQuery(document).ready(function($) {    
+
+                $('.btnBoxShadow').hover(function() {
+
+                    colTrans = "#ffffff";
+
+                    if ($(this).hasClass('btnCafe') ) {
+                        colTrans = "#110c0d";
+                    } 
+                    if ($(this).hasClass('btnVerde') ) {
+                        colTrans = "#08a635";
+                    } 
+                             
+
+                    wid = $(this).outerWidth();
+                    fondo = $(this).css("background-color");
+
+                    estilo = 'inset '+wid+'px 0px 0px '+colTrans;
+
+                    $(this).css('box-shadow', estilo); 
+                }, function() {
+                    $(this).css('box-shadow', ''); 
+                });                  
+
+                $(window).load(function() {     
+
+                    if (screen.width >= 768) {
+
+                    }
+                });              
+                
+            });
+        </script>
+    </body>
+</html>
